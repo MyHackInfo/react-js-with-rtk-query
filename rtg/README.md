6eee6ffbbf8708d01cd581d6fac514f982ed11f2
Narsi Gurjar <35182611+MyHackInfo@users.noreply.github.com>
2024-07-06 09:42:47 +0530

    init react project with rtk query

---
+++ rtg/README.md
@@ -0,0 +1,21 @@
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+### `npm test`
+
+### `build:prod`
+### `build:local`
+
+Builds the app for production to the `build` folder.\
+
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+### Analyzing the Bundle Size
+### `npm run analyze`
+
---
+++ rtg/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <small>You are running this application in <b>{process.env.REACT_APP_ENV}</b> mode.</small>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
